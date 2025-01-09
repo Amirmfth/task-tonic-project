d1e5a6287784cf161b32bce23682d4e2ca5bd8e2
Amirmfth <102855219+Amirmfth@users.noreply.github.com>
2025-01-09 22:39:26 +0330

    added verification google metatag to head

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="swC2FvmvEZwOlfTGZfnAGrSaSbuuDC56Th5ZBP_bi30"
+        />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
